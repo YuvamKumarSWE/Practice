92173ce089eacee3ee6737a13b46de5bb243e901
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-06-06 02:10:34 -0400

    first commit

---
+++ JS-counter/src/Setter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Setter(){
+
+    const [hours , setHours] = useState(0);
+    const [minutes , setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+
+    return(
+        <>
+
+            
+            
+        </>
+    );
+}
+
+export default Setter;
