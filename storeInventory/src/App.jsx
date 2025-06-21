af41aaba6873ccdc39d80b1c2c2961b91d7ee084
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-06-21 01:21:39 -0400

    F1 done: Buttons with update

--- storeInventory/src/App.jsx
+++ storeInventory/src/App.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import Button from "./Button";
 
 function App(){
+
+  let currentIndex = 2;
+
+  const [inventory, setInventory] = useState([
+    {
+      name: "Ramen",
+      quantity: 5, 
+      index: 1
+    },
+    {
+      name: "Knife",
+      quantity: 15, 
+      index: 2
+    }
+  ]);
+
+  function updateQuantity(name){
+    setInventory(prevInventory => 
+      prevInventory.map(item => 
+        item.name === name 
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  }
+
   return(
-    <>
-      
-    </>
+    <div className="flex gap-4 text-2xl p-4">
+    {
+      inventory.map((item, index) => (
+        <Button key={index} title={item.name} quantity={item.quantity} onClick={()=>updateQuantity(item.name)} />
+      ))
+    }
+    </div>
   );
 }
 
--- storeInventory/src/Button.jsx
+++ storeInventory/src/Button.jsx
@@ -1,10 +1,10 @@
 
-function Button({title , quantity}){
+function Button({title , quantity, onClick}){
   return(
     <>
-      <button className="flex flex-col cursor-pointer bg-black text-white border-2 rounded-xl ">
-        <h3>{title } :</h3>
-        <h5>{{quantity}}</h5>
+      <button onClick={onClick} className="flex p-2 cursor-pointer bg-black text-white border-2 rounded-xl ">
+        <h3>{title }: </h3>
+        <h5 className="ml-2 text-blue-500"> { quantity}</h5>
       </button>
     </>
   );
